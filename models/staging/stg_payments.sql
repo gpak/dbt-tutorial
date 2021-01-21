cbb338d483ee3269603ccf386504283111f951f4
Guen Pak <gpak86@gmail.com>
2021-01-21 03:22:17 +0000

    ran a test. all checks out

---
+++ models/staging/stg_payments.sql
@@ -0,0 +1,8 @@
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status as payment_status,
+    AMOUNT/100 as amount_dollars,
+    created
+from raw.stripe.payment
